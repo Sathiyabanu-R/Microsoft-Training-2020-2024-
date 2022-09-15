78b6cb32e3668aa3249c607c48997b6745845efb
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-09-15 15:29:20 +0530

    Create 15.AdditionOfIntegersWithDecimal.java

---
+++ Google Assignments/Assignment-1/15.AdditionOfIntegersWithDecimal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int d=sc.nextInt();
+    float s=sc.nextFloat();
+    float p=d+s;
+    System.out.print(p);
+  }
+}
+/* input :5 5.2 
+  output :10.2
+
+  input: 6 3.5 
+output: 9.5 */

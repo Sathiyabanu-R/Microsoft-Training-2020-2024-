9d4d97e60d748a377b38198f13a51409ed3df919
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-09-15 19:39:16 +0530

    Create 8.SumOfNaturalNumbers.java

---
+++ Google Assignments/Assignment-1/8.SumOfNaturalNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+    int d=sc.nextInt();
+    int s=d*(d+1)/2;
+    System.out.print(s); 
+  }
+}
+  /*input :5     input :2   
+    output :15   output :3
+  */

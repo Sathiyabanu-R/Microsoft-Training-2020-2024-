585717cca0381e6a2971d5ebbe35d4ad85a464eb
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-09-17 19:56:06 +0530

    Create 1.InsertIntoABinarySearchTree.java

---
+++ Microsoft/In-Class-Assignments/Week-4/1.InsertIntoABinarySearchTree.java
@@ -0,0 +1,29 @@
+class Solution {
+        
+public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            root = new TreeNode(val);
+            return root;
+        }
+        if(val < root.val){
+            root.left = insertIntoBST(root.left, val);
+        }
+        else{
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
+/*
+Example 1:
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
+Explanation: Another accepted tree is:
+
+Example 2:
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
+
+Example 3:
+Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+Output: [4,2,7,1,3,5]    */

1e2a25c3a7567c605f9b33e4ca21343356c521cf
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-09-17 15:27:41 +0530

    Update 16.TransposeOfMatrix.java

--- Google Assignments/Assignment-1/16.TransposeOfMatrix.java
+++ Google Assignments/Assignment-1/16.TransposeOfMatrix.java
@@ -1,1 +1,35 @@
-hjtyu
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int row=sc.nextInt();
+    int col=sc.nextInt();
+    int matrix[][]=new int[row][col];
+    int transpose[][]=new int[row][col];
+    for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+        matrix[i][j]=sc.nextInt();
+      }
+    }
+    for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+        transpose[j][i]=matrix[i][j];
+      }
+    }
+    for(int i=0;i<col;i++){
+      for(int j=0;j<row;j++){
+        System.out.print(transpose[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
+}
+ /*input : 3 3
+          1 2 3 
+          4 5 6
+          7 8 9
+   output:
+        1 4 7
+        2 5 8
+        3 6 9 */
+          

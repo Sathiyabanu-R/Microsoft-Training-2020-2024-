f800efb9bad38027c249ee656a691e9653bc3b1c
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-09-18 13:35:42 +0530

    Create 2.ReverseLinkedList-2.java

---
+++ Microsoft/Homework Assignments/Week-2/2.ReverseLinkedList-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+        // Empty list
+        if (head == null) {
+            return null;
+        }
+        ListNode cur = head, prev = null;
+        while (m > 1) {
+            prev = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+        ListNode con = prev, tail = cur;
+        ListNode third = null;
+        while (n > 0) {
+            third = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+        if (con != null) {
+       
+            con.next = prev;
+        } else {
+            head = prev;
+        }
+
+        tail.next = cur;
+        return head;
+    }
+}
+/*
+Example 1:
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [5], left = 1, right = 1
+Output: [5]  */
+ 
+

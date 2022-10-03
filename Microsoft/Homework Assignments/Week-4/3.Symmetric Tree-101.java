adb62f906c6a76212a0cab64b7cc54d786c7ca1d
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-10-03 12:38:30 +0530

    Create 3.Symmetric Tree-101.java

---
+++ Microsoft/Homework Assignments/Week-4/3.Symmetric Tree-101.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.right, right.left)
+                && isSymmetric(left.left, right.right);
+ 
+    }
+}
+/*
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false   */

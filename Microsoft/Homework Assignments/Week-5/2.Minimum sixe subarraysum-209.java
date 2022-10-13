42bb41cec15f4e269eacad7636521609fc6c561d
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-10-13 10:48:03 +0530

    Create 2.Minimum sixe subarraysum-209.java

---
+++ Microsoft/Homework Assignments/Week-5/2.Minimum sixe subarraysum-209.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l=nums.length;
+        int res=Integer.MAX_VALUE;
+        int left=0,sum=0;
+        for(int i=0;i<l;i++){
+            sum+=nums[i];
+            while(sum>=target){
+                res=Math.min(res,i-left+1);
+                sum-=nums[left];
+                left++;
+            }
+        }
+        return (res!=Integer.MAX_VALUE)?res:0;
+    }
+}
+/*
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+
+Example 3:
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0  */

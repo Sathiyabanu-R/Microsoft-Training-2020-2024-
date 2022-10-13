f71f250e2fe412f0e518be3470876acfc424ec02
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-10-13 11:49:19 +0530

    Create 6.Remove Nth node from end of List.java

---
+++ Microsoft/Homework Assignments/Week-2/6.Remove Nth node from end of List.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode left=dummy,right=dummy;
+        for(int i=1;i<=n+1;i++){
+            right=right.next;
+        }
+        while(right!=null){
+            left=left.next;
+            right=right.next;
+        }
+        left.next=left.next.next;
+        return dummy.next;
+    }
+}
+/*
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]  */

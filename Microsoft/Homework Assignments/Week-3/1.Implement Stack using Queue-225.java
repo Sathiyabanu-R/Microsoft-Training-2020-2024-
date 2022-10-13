6b3b1e5e44b4aecd181f3a8296b605a93433e41a
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-10-13 11:52:59 +0530

    Create 1.Implement Stack using Queue-225.java

---
+++ Microsoft/Homework Assignments/Week-3/1.Implement Stack using Queue-225.java
@@ -0,0 +1,42 @@
+class MyStack {
+    Queue<Integer> q;
+    public MyStack() {
+        q=new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        int size=q.size()-1;
+        while(size >0){
+            q.add(q.poll());
+            size--;
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+/*
+Example 1:
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False  */

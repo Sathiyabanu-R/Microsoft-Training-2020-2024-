750ad7dc406d2808c14e0bf88fa82f86a4053db2
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-11-01 19:58:14 +0530

    Create 16-238. Product of Array Except Self.java

---
+++ Question Bank Problems/Apple/16-238. Product of Array Except Self.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // first step is to calculate product of all non zero integers in the array
+        // also calculate frequency of zeroes in the array.
+        int product=1;
+        int flag=0;
+        for(int i:nums){   // here i indicates the value in the array.
+            if(i==0){ 
+                flag++;
+                continue;
+            }
+            product*=i;
+        }
+        for(int i=0;i<nums.length;i++){   // i indicates the index of nums
+          
+           // if all elements are non zero in the array, than product of all elements will be non-zero and every element will get a non-zero value after the calculations are over.
+              if(flag==0){
+                 nums[i]=product/nums[i];
+              }
+           // if zero appears only once, then for all the elements except that zero product of all alements will become zero.
+              else if(flag==1){
+                  nums[i]=nums[i]!=0 ? 0:product;
+              }
+            // if number of zeroes is more than 1, than for each elemrnt there will be an extra zero to make     product of other non zero elements equal to zero.
+              else{
+                 nums[i]=0;
+              }
+        }
+        return nums;
+    }
+}

2d663d7c831f979cf98643eb8ba3f06bfce6cade
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2022-11-01 19:48:23 +0530

    Create 8-2. Add Two Numbers.java

---
+++ Question Bank Problems/Apple/8-2. Add Two Numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp=new ListNode(0);
+        ListNode curr=temp;
+        int carry=0;
+        while(l1!=null || l2!=null || carry!=0){
+            int s=0;
+            if(l1!=null){
+                s+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                s+=l2.val;
+                l2=l2.next;
+            }
+            s+=carry;
+            carry=s/10;
+            ListNode node=new ListNode(s%10);
+            curr.next=node;
+            curr=curr.next;
+        }
+        return temp.next;
+    }
+}
